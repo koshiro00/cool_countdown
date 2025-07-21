285060b42298b886fbbad3bdd2acff9e9ca94f11
Koshiro Yukioka <130426310+koshiro00@users.noreply.github.com>
2025-07-21 09:03:48 +0900

    型を追加

---
+++ src/vite-env.d.ts
@@ -0,0 +1,105 @@
+/// <reference types="vite/client" />
+
+// CSS modules
+declare module "*.css" {
+  const content: Record<string, string>;
+  export default content;
+}
+
+declare module "*.scss" {
+  const content: Record<string, string>;
+  export default content;
+}
+
+declare module "*.sass" {
+  const content: Record<string, string>;
+  export default content;
+}
+
+declare module "*.less" {
+  const content: Record<string, string>;
+  export default content;
+}
+
+declare module "*.styl" {
+  const content: Record<string, string>;
+  export default content;
+}
+
+// Images
+declare module "*.svg" {
+  import * as React from "react";
+
+  export const ReactComponent: React.FunctionComponent<
+    React.SVGProps<SVGSVGElement> & { title?: string }
+  >;
+
+  const src: string;
+  export default src;
+}
+
+declare module "*.jpg" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.jpeg" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.png" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.gif" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.bmp" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.tiff" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.webp" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.ico" {
+  const src: string;
+  export default src;
+}
+
+// Fonts
+declare module "*.woff" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.woff2" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.eot" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.ttf" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.otf" {
+  const src: string;
+  export default src;
+}
