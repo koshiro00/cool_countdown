f996d7c56d2cc6c380dc1494fa6dfd0851ada8e2
Koshiro Yukioka <130426310+koshiro00@users.noreply.github.com>
2025-07-21 11:15:04 +0900

    reactように変更

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import { Analytics } from "@vercel/analytics/next";
+import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
